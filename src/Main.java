38b7e4c1d251312a8a1cd53499be4def520fcf26
KirGaard <kirgaard.kn@gmail.com>
2022-11-17 18:16:15 +0100

    Removed S.o.p from main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println("Kasper");
-
-        System.out.println("Omar");
-
-        System.out.println("Victor");
-
-        System.out.println("Loading Database");
 
 
     }
