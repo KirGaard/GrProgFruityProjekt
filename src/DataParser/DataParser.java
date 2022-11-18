7aa80a5d75a3628737a210c71df28d156777e390
KirGaard <kirgaard.kn@gmail.com>
2022-11-18 19:42:28 +0100

    Updated DataParser to parse series; Also DataParser now works with lines ending with a space Rollback film.txt to original file (Man måtte vist ikke ændre i data-filerne) Added parsedSeries.json

--- src/DataParser/DataParser.java
+++ src/DataParser/DataParser.java
@@ -14,39 +14,33 @@ public class DataParser {
         String finalString = "{\"list\":[";
 
         while (sc.hasNextLine()){
-            String line = sc.nextLine();
+            String line = RemoveUnnecessarySpaces(sc.nextLine());
             String jsonString = "{";
-            String[] element = line.split(";");
+            String[] DataElements = line.split(";");
             String[] categories = new String[]{
                     "title",
                     "releaseDate",
                     "genre",
                     "rating"
             };
-            for (int i = 0; i < element.length; i++) {
+            for (int i = 0; i < categories.length; i++) {
                 jsonString += '"' + categories[i] + '"' + ":";
-                if (i == 0){ // Title string case
-                    jsonString += '"' + element[i] + '"';
+                switch (i){
+                    case 0:
+                        jsonString += getString(DataElements[i]);
+                        break;
+                    case 2:
+                        jsonString += getList(DataElements[i]);
+                        break;
+                    default:
+                        jsonString += getNumber(DataElements[i]);
+                        break;
                 }
-                else if (i == 2){ // our genres - making it a list
-                    String[] genres = element[i].replaceAll(" ", "").split(",");
-                    jsonString += "[";
-                    for (int j = 0; j < genres.length; j++) {
-                        String genre = genres[j];
-                        jsonString += '"' + genre + '"';
-                        if (j < genres.length - 1) jsonString+= ",";
-                    }
-                    jsonString += "]";
-                }else{ // Our number cases
-                    String num = element[i].replaceAll(",", ".");
-                    jsonString += num;
-                }
-                if (i < element.length - 1) jsonString += ",";
+                if (i < categories.length - 1) jsonString += ",";
 
 
             }
             jsonString += "},";
-
             finalString += jsonString + "\n";
 
 
@@ -56,7 +50,75 @@ public class DataParser {
         writeToJSONFile("parsedFilm", finalString);
     }
 
-        private static void writeToJSONFile(String filename, String input){
+
+
+
+    public static void seriesTxtToJSON() throws FileNotFoundException {
+        File file = new File("Data\\serier.txt");
+        Scanner sc = new Scanner(file);
+        String finalString = "{\"list\":[";
+
+        while (sc.hasNextLine()){
+            String line = RemoveUnnecessarySpaces(sc.nextLine());
+            String jsonString = "{";
+            String[] dataElements = line.split(";");
+            String[] categories = new String[]{
+                    "title",
+                    "runTime",
+                    "genre",
+                    "rating",
+                    "seasons"
+            };
+            for (int i = 0; i < categories.length; i++) {
+                jsonString += '"' + categories[i] + '"' + ":";
+                switch (i){
+                    case 0: case 1:
+                        jsonString += getString(dataElements[i]);
+                        break;
+                    case 2: case 4:
+                        jsonString += getList(dataElements[i]).replaceAll("[0-9]-", "");
+                        break;
+                    default:
+                        jsonString += getNumber(dataElements[i]);
+                        break;
+                }
+                if (i < categories.length - 1) jsonString += ",";
+            }
+            jsonString += "},";
+            finalString += jsonString + "\n";
+
+
+        }
+        finalString = finalString.replaceAll(".$", "");
+        finalString += "]}";
+        writeToJSONFile("parsedSeries", finalString);
+    }
+
+    private static String getString(String element) {
+        return '"' + element + '"';
+
+    }
+    private static String getNumber(String element) {
+        return element.replaceAll(",", ".");
+
+    }
+
+    private static String getList(String element) {
+        String[] genres = element.replaceAll(" ", "").split(",");
+        String out = "";
+
+        out += "[";
+        for (int j = 0; j < genres.length; j++) {
+            String genre = genres[j];
+            out += '"' + genre + '"';
+            if (j < genres.length - 1) out += ",";
+        }
+        out += "]";
+        return out;
+    }
+
+
+    private static void writeToJSONFile(String filename, String input){
             try {
                 FileWriter fileWriter = new FileWriter("Data\\" + filename+ ".json");
                 fileWriter.write(input);
@@ -65,9 +127,14 @@ public class DataParser {
                 System.out.println("An error occurred creating JsonFile.");
                 e.printStackTrace();
             }
-        }
-
     }
+
+    private static String RemoveUnnecessarySpaces(String string){
+        return string.replaceAll("; *", ";");
+    }
+
+
+}
 
 
 
