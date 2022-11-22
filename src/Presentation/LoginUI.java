1e361cf8af128323fadd46f026e68b048eeac56c
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-11-22 14:04:38 +0100

    Added classes LoginUI, DisplayUI and OverviewUI

--- src/Presentation/LoginUI.java
+++ src/Presentation/LoginUI.java
@@ -1,31 +1,13 @@
 package Presentation;
 
-import java.util.Objects;
-import java.util.Scanner;
-
 public class LoginUI implements IPresenter {
     public static void main(String[] args) {
-        LoginUI loginUI = new LoginUI();
-        loginUI.init();
+        
     }
 
     @Override
     public void init() {
-        System.out.println("Welcome to iFruity, please provide login information");
-        System.out.println("Username:");
-        Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
-        if (Objects.equals(username, "")) {
-            System.out.println("Login failed");
-            throw new NullPointerException("Invalid username");
-        }
-        System.out.println("Password:");
-        String password = scanner.nextLine();
-        if (Objects.equals(password, "")) {
-            System.out.println("Login failed");
-            throw new NullPointerException("Invalid password");
-        }
-        System.out.println("Login success");
+
     }
 
     @Override
