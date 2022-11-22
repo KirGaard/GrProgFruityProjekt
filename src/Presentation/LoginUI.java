f717c85d1e60b6f62bb4b228457c59d51b682893
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-11-22 14:04:38 +0100

    Added classes LoginUI, DisplayUI and OverviewUI

---
+++ src/Presentation/LoginUI.java
@@ -0,0 +1,17 @@
+package Presentation;
+
+public class LoginUI implements IPresenter {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
---
+++ src/Presentation/OverviewUI.java
@@ -0,0 +1,13 @@
+package Presentation;
+
+public class OverviewUI implements IPresenter {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
