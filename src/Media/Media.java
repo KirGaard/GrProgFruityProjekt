a373f8673196e08bae0a8d5851069f4b432e4afa
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-11-22 13:42:08 +0100

    Added abstract class Media

--- src/Media/Media.java
+++ src/Media/Media.java
@@ -1,4 +1,14 @@
 package Media;
 
-public class Media {
+import java.util.List;
+
+public abstract class Media {
+    protected String title;
+    protected String release;
+    protected double rating;
+    protected List<String> genre;
+
+    String getInfo() {
+        throw new RuntimeException();
+    }
 }
