121d47cc1ac4d103f087235eca70a9e92e99a310
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-11-22 16:32:40 +0100

    Added media, film and show methods

--- src/Media/Film.java
+++ src/Media/Film.java
@@ -6,17 +6,4 @@ public class Film extends Media {
     public String getInfo() {
         throw new RuntimeException();
     }
-
-    public String getTitle() {
-        return title;
-    }
-    public String getRelease() {
-        return release;
-    }
-    public double getRating() {
-        return rating;
-    }
-    public List<String> getGenre() {
-        return genre;
-    }
 }
