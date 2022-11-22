c733645b8940e751251464fd27f5db7090654d2d
Omar <omarkhalil16@hotmail.com>
2022-11-22 13:51:02 +0100

    Added class User fields and methods

--- src/User/User.java
+++ src/User/User.java
@@ -1,4 +1,32 @@
 package User;
 
+import java.util.List;
+
 public class User {
+    private String name;
+    private String password;
+    private List<String> favoriteTitles;
+
+    public void addFavorite(String favorite) {
+        favoriteTitles.add(favorite);
+    }
+
+    public void getFavorites() {
+        for (String favorite : favoriteTitles) {
+            System.out.println(favorite);
+        }
+    }
+
+    public void removeFavorite (String favorite) {
+        favoriteTitles.remove(favorite);
+    }
+
+    public void changeUsername (String name) {
+        this.name = name;
+    }
+
+    public void changePassword (String password) {
+        this.password = password;
+    }
 }
+
