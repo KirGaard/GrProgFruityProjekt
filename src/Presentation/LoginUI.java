ae6305ad796697ddcd45b0c15168013b55841ace
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-11-22 14:23:00 +0100

    Added login implementation test

--- src/Presentation/LoginUI.java
+++ src/Presentation/LoginUI.java
@@ -10,7 +10,19 @@ public class LoginUI implements IPresenter {
 
     @Override
     public void init() {
-
+        System.out.println("Welcome to iFruity, please provide the according username and password for the user you want to use");
+        System.out.println("Username:");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        if (username == "") {
+            throw new NullPointerException("Invalid username");
+        }
+        System.out.println("Password:");
+        String password = scanner.nextLine();
+        if (password == "") {
+            throw new NullPointerException("Invalid password");
+        }
+        System.out.println("Login success");
     }
 
     @Override
