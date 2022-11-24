42a30f517d385a27255b0e3e3404e0511cbd216d
Omar <omarkhalil16@hotmail.com>
2022-11-24 13:57:11 +0100

    Implemented a constructor for DisplayUI

--- src/Presentation/DisplayUI.java
+++ src/Presentation/DisplayUI.java
@@ -2,10 +2,17 @@ package Presentation;
 
 import Database.MediaDatabase;
 import Database.MediaNotInDatabaseException;
+import Media.Media;
 
 import java.util.Scanner;
 
 public class DisplayUI implements IPresenter {
+    private Media media;
+
+    DisplayUI(Media media) {
+        this.media = media;
+    }
+
     @Override
     public void init() {
         System.out.println(media.getInfo());
