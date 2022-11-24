5d57db7d349d6474d828bb5d929d6768eeae16ae
Omar <omarkhalil16@hotmail.com>
2022-11-24 14:01:29 +0100

    Formatted getInfo for shows

--- src/Media/Show.java
+++ src/Media/Show.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Show extends Media {
     private List <String> seasons;
     public String getInfo() {
-        return title + " " + release + " " + genre + " " + rating + " " + seasons;
+        return "Title: " + title + " Release date: " + release + " Genre: " + genre + " Rating: " + rating + " Seasons: " + seasons;
     }
 }
