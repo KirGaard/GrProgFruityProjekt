ca670c6c9aa6a4231f13dda29a700e84de98812c
Omar <omarkhalil16@hotmail.com>
2022-11-24 14:06:22 +0100

    Added updated show and film to presentaion

--- src/Media/Show.java
+++ src/Media/Show.java
@@ -3,8 +3,8 @@ package Media;
 import java.util.List;
 
 public class Show extends Media {
-    private List<String> seasons;
+    private List <String> seasons;
     public String getInfo() {
-        return title + " " + release + " " + genre + " " + rating + " " + seasons;
+        return "Title: " + title + " Release date: " + release + " Genre: " + genre + " Rating: " + rating + " Seasons: " + seasons;
     }
 }
