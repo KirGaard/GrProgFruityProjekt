a95093cb62b3fee5172bab4cab46e98b1c70997a
KirGaard <kirgaard.kn@gmail.com>
2022-12-08 23:32:36 +0100

    Bgean work on overview

---
+++ src/Presentation/Overview/OverviewController.java
@@ -0,0 +1,13 @@
+package Presentation.Overview;
+
+import javafx.fxml.FXML;
+
+public class OverviewController {
+
+    @FXML
+    private void initialize()
+    {
+        System.out.println("Initializing Overview");
+    }
+
+}
