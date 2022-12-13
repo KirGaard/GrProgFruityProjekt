8322cbd70f2a434ada478684f95fe6ad70634564
Gooomer <117921091+Gooomer@users.noreply.github.com>
2022-12-13 15:33:58 +0100

    Added UsernameAlreadyExistsException and InvalidInputException and changes to NewUserController and LoginController

--- src/Presentation/Login/LoginController.java
+++ src/Presentation/Login/LoginController.java
@@ -2,24 +2,16 @@ package Presentation.Login;
 
 import Database.UserDatabase;
 import Presentation.IController;
+import Presentation.Login.Exceptions.InvalidInputException;
 import Presentation.Main;
 import User.UserPrefs;
-import javafx.beans.InvalidationListener;
-import javafx.beans.value.ChangeListener;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.stage.Window;
 
-import java.awt.*;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 
 public class LoginController implements IController {
@@ -54,7 +46,7 @@ public class LoginController implements IController {
 
         try {
             validateLogin(username, password);
-        } catch (InvalidUserException e) {
+        } catch (InvalidInputException e) {
             usernameField.setText("");
             passwordField.setText("");
             wrongInput.setVisible(true);
@@ -69,10 +61,10 @@ public class LoginController implements IController {
         }
     }
 
-    private void validateLogin(String username, String password) throws InvalidUserException {
+    private void validateLogin(String username, String password) throws InvalidInputException {
 
-        if (!userDatabase.ContainsUser(username)) { throw new InvalidUserException(); }
-        if (!userDatabase.getUser(username).getPassword().equals(password)) { throw new InvalidUserException(); }
+        if (!userDatabase.ContainsUser(username)) { throw new InvalidInputException(); }
+        if (!userDatabase.getUser(username).getPassword().equals(password)) { throw new InvalidInputException(); }
     }
 
 }
--- src/Presentation/Login/NewUserController.java
+++ src/Presentation/Login/NewUserController.java
@@ -1,6 +1,7 @@
 package Presentation.Login;
 
 import Presentation.IController;
+import Presentation.Login.Exceptions.UsernameAlreadyExistsException;
 import Presentation.Main;
 import User.User;
 import javafx.event.ActionEvent;
@@ -24,10 +25,14 @@ private Label errorLabel;
 
     @Override
     public void exit() {
-        CreateNewUser();
+        try {
+            CreateNewUser();
+        } catch (UsernameAlreadyExistsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void CreateNewUser(){
+    private void CreateNewUser() throws UsernameAlreadyExistsException {
         String username = usernameField.getText().toLowerCase();
         String password = passwordField.getText();
 
@@ -36,7 +41,7 @@ private Label errorLabel;
 
         if (Main.userDatabase.ContainsUser(username)){
             errorLabel.setText("Navn eksisterer allerede!");
-            return;
+            throw new UsernameAlreadyExistsException();
         }
 
         Pattern legalPattern = Pattern.compile("^([a-z]|[0-9]|[?!])+$");
